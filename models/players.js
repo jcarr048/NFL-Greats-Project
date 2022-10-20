a4a3bfc4b10a6d5324f0448fe342169b08d3271b
Jeff Carroll <jcarr048@gmail.com>
2022-10-20 11:35:29 -0400

    removed node

--- models/players.js
+++ models/players.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-const Players = new Schema({
-  image: { type: String, required: true },
-  name: { type: String, required: true },
-  yearsPlayed: { type: String, required: true },
-  college: { type: String, required: true },
-  awards: [{ type: String, required: true }],
-  position: { type: String, required: true },
-  proFootballRef: { type: String, required: true }
-})
+const Players = new Schema(
+  {
+    image: { type: String, required: true },
+    team: { type: Schema.Teams.Object.Id, ref: 'Team' },
+    name: { type: String, required: true },
+    yearsPlayed: { type: String, required: true },
+    college: { type: String, required: true },
+    awards: { type: Array, required: true },
+    position: { type: String, required: true },
+    proFootballRef: { type: String, required: true }
+  },
+  { timestamps: true }
+)
 
 module.exports = mongoose.model('Player', Players)
