d5cbfbe00c87bf0f164a183bd12ef7069fd87e8b
Jeff Carroll <jcarr048@gmail.com>
2022-10-20 12:29:14 -0400

    Working on DBs

--- models/players.js
+++ models/players.js
@@ -4,12 +4,12 @@ const Schema = mongoose.Schema
 const Players = new Schema(
   {
     image: { type: String, required: true },
-    team: { type: Schema.Teams.Object.Id, ref: 'Team' },
+    team: { type: Schema.Teams.objectId, ref: 'Team' },
     name: { type: String, required: true },
+    position: { type: String, required: true },
     yearsPlayed: { type: String, required: true },
     college: { type: String, required: true },
     awards: { type: Array, required: true },
-    position: { type: String, required: true },
     proFootballRef: { type: String, required: true }
   },
   { timestamps: true }
---
+++ seed/player.js
@@ -0,0 +1,17 @@
+const db = require()
+
+const playerPage = async () => {
+  const teamPage = await Team.find({ name: })
+const player = [
+  {
+   image: 'https://www.pro-football-reference.com/req/20180910/images/headshots/BreeDr00_2020.jpg',
+   team: 
+   name: 'Drew Brees',
+   position: 'Quarterback',
+   yearsPlayed: 2001-2020,
+   college: 'Purdue',
+   awards: [proBowls: 13, superBowls: 1, offensivePoy: 2, allPro: 1],
+   proFootballRef: 'https://www.pro-football-reference.com/players/B/BreeDr00.htm'
+  }
+]
+}
