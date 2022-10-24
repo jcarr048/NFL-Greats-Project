141f2bb1849d750fd67fe91b7026a8959182ab10
Jeff Carroll <jcarr048@gmail.com>
2022-10-24 13:56:25 -0400

    Working on routes

---
+++ controllers/index.js
@@ -0,0 +1,66 @@
+const Player = require('../models/players')
+const Team = require('../models/teams')
+
+const createPlayer = async (req, res) => {
+  try {
+    const player = await new Player(req.body)
+    await player.save()
+    return res.status(201).json({
+      player
+    })
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
+
+const getAllTeams = async (req, res) => {
+  try {
+    const teams = await Team.find()
+    return res.status(200).json({ teams })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const getAllPlayers = async (req, res) => {
+  try {
+    const players = await Player.find()
+    return res.status(200).json({ players })
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const getPlayerById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const player = await Player.findById(id)
+    if (player) {
+      return res.status(200).json({ player })
+    }
+    return res.status(404).send('Player with specific ID does not exist')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const getTeamById = async (req, res) => {
+  try {
+    const { id } = req.params
+    const team = await Team.findById(id)
+    if (team) {
+      return res.status(200).json({ team })
+    }
+    return res.status(404).send('Team with specific ID cannot be found')
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+module.exports = {
+  createPlayer,
+  getAllPlayers,
+  getPlayerById,
+  getAllTeams,
+  getTeamById
+}
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,17 @@
 const { Router } = require('express')
+const controllers = require('../controllers')
 const router = Router()
+
+router.get('/', (req, res) => res.send('This works!'))
+
+router.post('/players', controllers.createPlayer)
+
+router.get('/players', controllers.getAllPlayers)
+
+router.get('/players/:id', controllers.getPlayerById)
+
+router.get('/teams', controllers.getAllTeams)
+
+router.get('/teams/:id', controllers.getTeamById)
 
 module.exports = router
