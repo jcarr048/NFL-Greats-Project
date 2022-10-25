4b64ba99d2f88b4a9f020e231fb9d5b5b5cbf4fe
Jeff Carroll <jcarr048@gmail.com>
2022-10-25 18:20:13 -0400

    Finally have front page working, somewhat

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Header from './components/Header'
+import About from './pages/About'
+import PlayerPage from './pages/PlayerPage'
+import TeamPage from './pages/TeamPage'
+import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/player/playerId" element={<PlayerPage />} />
+          <Route path="/teams/teamId" element={<TeamPage />} />
+        </Routes>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ client/src/pages/Home.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+import TeamInfo from '../components/TeamInfo'
+const Home = () => {
+  const [teams, setTeams] = useState([])
+
+  const getTeams = async () => {
+    const response = await axios.get(`http://localhost:3001/api/teams`)
+    setTeams(response.data.results)
+  }
+  useEffect(() => {
+    getTeams()
+  })
+  return (
+    <div>
+      <div className="teams">
+        <h2>Teams</h2>
+        <section className="container-grid">
+          {teams.map((result) => (
+            <Link to={`/view/${result.id}`} key={result.id}>
+              <TeamInfo
+                image={result.image}
+                name={result.name}
+                division={result.division}
+              />
+            </Link>
+          ))}
+        </section>
+      </div>
+    </div>
+  )
+}
+
+export default Home
