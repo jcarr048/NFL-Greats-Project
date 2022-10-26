c46d2a7e757966a64d159a46e1620916dce1f046
Jeff Carroll <jcarr048@gmail.com>
2022-10-26 09:08:36 -0400

    Fixed

--- db/index.js
+++ db/index.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-  .connect(
-    'mongodb+srv://j3ppy:Irishrugby12@nfldatabase.6efkrgd.mongodb.net/?retryWrites=true&w=majority'
-  )
+  .connect('mongodb://127.0.0.1:27017/nflDatabase')
   .then(() => {
     console.log('Successfully connected to MongoDB.')
   })
