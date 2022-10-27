d5cd95b3a6333b418605392a7eb252de20153b5d
Jeff Carroll <jcarr048@gmail.com>
2022-10-27 12:16:30 -0400

    Hope this works

--- db/index.js
+++ db/index.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose')
 
+let MONGODB_URI =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : 'mongodb://127.0.0.1:27017/nflDatabase'
+
 mongoose
-  .connect('mongodb://127.0.0.1:27017/nflDatabase')
+  .connect(MONGODB_URI)
   .then(() => {
     console.log('Successfully connected to MongoDB.')
   })
